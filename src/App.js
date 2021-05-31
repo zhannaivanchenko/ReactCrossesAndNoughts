a88134499524cb46a7629f52e67643fd38f35c6e
Zhanna Ivanchenko <zhannaivanchenko@Zhannas-MacBook-Pro.local>
2021-05-31 12:14:05 +0300

    Update responsive design

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
-import './App.css';
-import { Matrix } from "./Components/Matrix/Matrix";
 import React, { useState } from 'react';
 import { Header } from "./Components/Header/Header";
+import { Matrix } from "./Components/Matrix/Matrix";
 
-const createBlankTable = [
+const createBlankTable = () => [
   [0, 0, 0],
   [0, 0, 0],
   [0, 0, 0]
 ];
 
+
 export function App() {
-  const [ db, setDb ] = useState(JSON.parse(JSON.stringify(createBlankTable)));
+  const [db, setDb] = useState(createBlankTable());
 
   const setNewDb = (x, y, value) => {
     let newDb = [...db];
     newDb[x][y] = value;
     setDb(newDb);
   };
- 
+
   const cleanDb = () => {
-    const newBlankTable = JSON.parse(JSON.stringify(createBlankTable));
+    const newBlankTable = createBlankTable();
     setDb(newBlankTable);
   };
 
-  const putCross = (x, y) => {     
+  const putCross = (x, y) => {
     const isCellEmpty = db[x][y] === 0;
-    if (!isCellEmpty) {  alert('This cell is clicked! Click another one!') }
-    if ( isCellEmpty ) {
+    if (!isCellEmpty) { alert('This cell is clicked! Click another one!') }
+    if (isCellEmpty) {
       setNewDb(x, y, 1);
-    }      
+    }
   }
 
   return (
-      <div>
-        <Header /> 
-        <Matrix matrix={ [...db] } putCross={putCross} cleanDb={cleanDb} setNewDb={setNewDb} />
-      </div>
-    );
+    <div>
+      <Header />
+      <Matrix matrix={[...db]} putCross={putCross} cleanDb={cleanDb} setNewDb={setNewDb} />
+    </div>
+  );
 };
 
 
